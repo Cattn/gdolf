bdfbb28fa6805f8a0f8217dcf656f9f1dd61a1bc
Cattn <Logan@cattn.dev>
2025-08-20 17:20:02 -0400

    shot indicator next to mouse

--- raycast.gd
+++ raycast.gd
@@ -2,11 +2,16 @@ extends Node2D
 const Find = preload("res://utilscripts/find.gd")
 
 @onready var ray = $RayCast2D
-@onready var ball = Find.find_ball(self)  # Reference to ball
+@onready var ball = Find.find_ball(self)
+@onready var turn_manager: Node = Find.find_turn_manager(self)
+@onready var tex_yes: Texture2D = load("res://Ball/YesBall.png")
+@onready var tex_no: Texture2D = load("res://Ball/NoBall.png")
 
 func _ready():
 	if not ball:
 		ball = Find.find_ball(self)
+	if not turn_manager:
+		turn_manager = Find.find_turn_manager(self)
 
 @export var line_length: float = 150.0
 
@@ -16,6 +21,8 @@ func _process(_delta):
 	if not ray:
 		return
 	ball = Find.find_ball(self)
+	if not turn_manager:
+		turn_manager = Find.find_turn_manager(self)
 	if not ball:
 		return
 	
@@ -53,6 +60,15 @@ func _draw():
 		var collision_point = ray.get_collision_point()
 		draw_circle(to_local(collision_point), 5, Color.YELLOW)
 
+	if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		var can_shoot := _can_shoot()
+		var icon := tex_yes if can_shoot else tex_no
+		if icon:
+			var cursor_local = to_local(mouse_pos)
+			var icon_pos = cursor_local + Vector2(20.0, -20.0)
+			var icon_size = Vector2(icon.get_width(), icon.get_height()) / 3.0
+			draw_texture_rect(icon, Rect2(icon_pos, icon_size), false)
+
 func draw_power_bar():
 	if not ball or not ball.has_method("get_current_power"):
 		return
@@ -61,27 +77,22 @@ func draw_power_bar():
 	if power_level <= 0:
 		return
 	
-	# Power bar properties
+	# hit power bar
 	var max_bar_length = 60.0
-	var cursor_offset = Vector2(30.0, 0.0)  # Fixed offset to the right of cursor
+	var cursor_offset = Vector2(30.0, 0.0) 
 	var bar_thickness = 4.0
 	
-	# Get cursor position relative to ball
 	var mouse_pos = get_global_mouse_position()
 	var cursor_local = to_local(mouse_pos)
 	
-	# Position bar to the right of cursor
 	var bar_start = cursor_local + cursor_offset
 	var bar_length = power_level * max_bar_length
-	var bar_end = bar_start + Vector2(0.0, bar_length)  # Vertical bar
+	var bar_end = bar_start + Vector2(0.0, bar_length) 
 	
-	# Color changes from green to red based on power
 	var power_color = Color.GREEN.lerp(Color.RED, power_level)
 	
-	# Draw the power bar
 	draw_line(bar_start, bar_end, power_color, bar_thickness)
 	
-	# Draw a background line to show max power
 	var max_end = bar_start + Vector2(0.0, max_bar_length)
 	draw_line(bar_start, max_end, Color.GRAY, 2.0)
 
@@ -93,3 +104,12 @@ func get_aim_direction() -> Vector2:
 		return direction_vector.normalized()  
 	else:
 		return Vector2.DOWN
+
+func _can_shoot() -> bool:
+	if not ball:
+		return false
+	if turn_manager and turn_manager.has_method("can_start_shot"):
+		return turn_manager.can_start_shot()
+	var lin_ok: bool = ball.linear_velocity.length() <= 0.1
+	var ang_ok: bool = abs(ball.angular_velocity) <= 0.1
+	return lin_ok and ang_ok
